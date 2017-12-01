5e457466caa271d3629007ecef8b2c3eea6ab723
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-01 01:06:42 -0800

    playing with setTimeout

--- Starter/app.js
+++ Starter/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', '$timeout', function($scope, $filter, $timeout) {
+myApp.controller('mainController', ['$scope', '$filter', '$timeout','$log', function($scope, $filter, $timeout, $log) {
     
   $scope.handle = '';
   
@@ -8,12 +8,30 @@ myApp.controller('mainController', ['$scope', '$filter', '$timeout', function($s
     return $filter('lowercase')($scope.handle);
   };
   
+  //This don't work
+  $scope.lhandle = $filter('lowercase')($scope.handle);
+
   //Runs this function when handle changes
   $scope.$watch('handle', function(newValue, oldValue) {
+    $log.log($scope.lhandle);
     console.info('Changed!');
     console.log('Old:' + oldValue);
     console.log('New:' + newValue);
   });
+
+
+  // $scope.$watch('lhandle', function(newValue, oldValue) {
+  //   console.info('Changed!');
+  //   console.log('Old:' + oldValue);
+  //   console.log('New:' + newValue);
+  // });
     
+  //Doesn't change the scope even though it ran because it is outside AngularJs context.
+  //Never started the digest loop
+  setTimeout(function(){
+    $scope.handle = 'hi';
+    $log.log('Scope changed');
+  }, 3000);
+
     
 }]);
